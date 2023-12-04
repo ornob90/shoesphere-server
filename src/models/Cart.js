c4d81022e2256fbf69a433ee5af62ea4cda300b8
ornob90 <oornob49@gmail.com>
2023-12-04 19:02:30 +0600

    Cart model complete

---
+++ src/models/Cart.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const cartSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  addDate: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+});
+
+const Cart = mongoose.model("Cart", cartSchema);
+
+module.exports = Cart;
