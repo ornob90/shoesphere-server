28e0bf70e938ab60639126dc412c9c2b311485bc
ornob90 <oornob49@gmail.com>
2023-12-04 19:20:55 +0600

    order model complete

---
+++ src/models/order.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  orderDate: {
+    type: Date,
+    default: Date.now,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: "pending",
+  },
+});
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
