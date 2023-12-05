58061addf23910820ef0e0b4949b7b2d1c5317d4
ornob90 <oornob49@gmail.com>
2023-12-05 19:16:02 +0600

    get new arrivals api complete

--- src/api/product/getNewArrivals.js
+++ src/api/product/getNewArrivals.js
@@ -1,5 +1,14 @@
+const Product = require("../../models/Product");
+
 const getNewArrivals = async (req, res, next) => {
   try {
+    const { size } = req.query || { size: 10 };
+    const products = await Product.find({})
+      .sort({ addedDate: 1 })
+      .limit(size)
+      .select("images name");
+
+    res.send(products);
   } catch (error) {
     console.log(error.message);
     next(error);
