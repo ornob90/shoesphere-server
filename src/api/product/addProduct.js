866e1df74e426071ac433213ad8bc393bb1c276b
ornob90 <oornob49@gmail.com>
2023-12-05 19:58:10 +0600

    brand add based on product

--- src/api/product/addProduct.js
+++ src/api/product/addProduct.js
@@ -1,14 +1,26 @@
 const Product = require("../../models/Product");
+const Brand = require("../../models/brand");
 
 const addProduct = async (req, res, next) => {
   try {
     const data = req.body;
 
     const product = new Product(data);
+
+    const brand = {
+      name: product.brand,
+      logo: product.brandLogo,
+    };
+
+    const response = await Brand.updateOne({ name: brand.name }, brand, {
+      upsert: true,
+    });
+
     await product.save();
 
     res.send({
       insertOne: true,
+      newBrand: response.modifiedCount > 0,
     });
   } catch (error) {
     console.log(error.message);
---
+++ src/models/brand.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const brandSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  logo: {
+    type: String,
+    required: true,
+  },
+});
+
+const Brand = mongoose.model("Brand", brandSchema);
+
+module.exports = Brand;
